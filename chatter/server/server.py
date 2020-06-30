4950d8e72373d81d6e1891f7a5275f56dcb8d3c1
Bishal <bishal1banerjee@gmail.com>
2020-06-30 20:35:02 +0530

    Got code from tech with tim

--- chatter/server/server.py
+++ chatter/server/server.py
@@ -3,83 +3,87 @@ from threading import Thread
 import time
 from person import Person
 
-
 # GLOBAL CONSTANTS
-HOST = 'localhost'
+HOST = ''
 PORT = 5500
-BUFSIZ = 512
 ADDR = (HOST, PORT)
-MAX_CONNECTIONS = 10
+MAX_CONNETIONS = 10
+BUFSIZ = 512
 
 # GLOBAL VARIABLES
 persons = []
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)  # set up server
 
+
 def broadcast(msg, name):
     """
-    Send new messages to all clients
+    send new messages to all clients
     :param msg: bytes["utf8"]
     :param name: str
     :return:
     """
     for person in persons:
         client = person.client
-        client.send(bytes(name + ": ", "utf8") + msg)
+        try:
+            client.send(bytes(name, "utf8") + msg)
+        except Exception as e:
+            print("[EXCEPTION]", e)
 
 
 def client_communication(person):
     """
-    THREAD TO HANDLE ALL MESSAGES FROM CLIENT
-    ;param person: Person
-    ;return: None
+    Thread to handle all messages from client
+    :param person: Person
+    :return: None
     """
-    run = True
     client = person.client
-    name = person.name
-    # addr = person.addr
 
+    # first message received is always the persons name
     name = client.recv(BUFSIZ).decode("utf8")
+    person.set_name(name)
+
     msg = bytes(f"{name} has joined the chat!", "utf8")
-    broadcast(msg, name) # Broadcast welcome message
-    while run:
-        try:
-            msg = person.recv(BUFSIZ)
-            print(f"{name}: ", msg.decode("utf8"))
-            if msg != bytes("{quit}", "utf8"):
-                broadcast(f"{name} has left the chat.", "")
-                person.close()
-                persons.remove(person)
-            else:
-                broadcast(msg, name)
-        except Exception as e:
-            print("[EXCEPTION]", e)
+    broadcast(msg, "")  # broadcast welcome message
+
+    while True:  # wait for any messages from person
+        msg = client.recv(BUFSIZ)
+
+        if msg == bytes("{quit}", "utf8"):  # if message is qut disconnect client
+            client.close()
+            persons.remove(person)
+            broadcast(bytes(f"{name} has left the chat...", "utf8"), "")
+            print(f"[DISCONNECTED] {name} disconnected")
             break
+        else:  # otherwise send message to all other clients
+            broadcast(msg, name+": ")
+            print(f"{name}: ", msg.decode("utf8"))
 
 
 def wait_for_connection():
     """
-    WAIT FOR CONNECTION FROM NEW CLIENTS, STRT NEW THREAD ONCE CONNECTED
-    :param SERVER: SOCKET
-    :return: NONE
+    Wait for connecton from new clients, start new thread once connected
+    :return: None
     """
-    run = True
-    while run:
+
+    while True:
         try:
-            client, addr = SERVER.accept()
-            person = Person(addr, client)
+            client, addr = SERVER.accept()  # wait for any new connections
+            person = Person(addr, client)  # create new person for connection
             persons.append(person)
-            print(f"[CONNECTION]: {addr} connected to server at {time.time()}")
+
+            print(f"[CONNECTION] {addr} connected to the server at {time.time()}")
             Thread(target=client_communication, args=(person,)).start()
         except Exception as e:
             print("[EXCEPTION]", e)
-            run = False
-    print("SERVER CRASHED!!!!")
+            break
+
+    print("SERVER CRASHED")
 
 
 if __name__ == "__main__":
-    SERVER.listen(MAX_CONNECTIONS)
-    print("[STARTED] Waiting for connection...")
+    SERVER.listen(MAX_CONNETIONS)  # open server to listen for connections
+    print("[STARTED] Waiting for connections...")
     ACCEPT_THREAD = Thread(target=wait_for_connection)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
